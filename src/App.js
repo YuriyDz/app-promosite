3351a2c2a708fe296e198f159f6f19968eb0a6f6
YuriyDz <dziapko.yurii@student.uzhnu.edu.ua>
2023-10-09 09:22:42 +0300

    actions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>hnjggybhnf</code> and save to reload.
+          Edit <code>ьн куфсл</code> and save to reload.
         </p>
         <a
           className="App-link"

5624f70fc82181d0376937d93f82c54458da6a3f
YuriyDz <dziapko.yurii@student.uzhnu.edu.ua>
2023-12-26 10:06:05 +0200

    Aded two icons and updatetes two buttons

--- src/components/ButtonSubmit/butthonSubmit.jsx
+++ src/components/ButtonSubmit/butthonSubmit.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./butthonSubmit.css";
 export const ButtonSubmit = ({isSend,doSend}) => {
     if(isSend == true){
-        return <button className="button4" onClick={() => doSend()}>send</button>;
+        return <button className="button4" onClick={() => doSend()}><img src="https://cdn-icons-png.flaticon.com/512/2099/2099085.png" height="20" width="20"></img></button>;
     }
-    return <button className="button4" onClick={() => doSend()}>OK</button>;
+    return <button className="button4" onClick={() => doSend()}><img src="https://simpleicon.com/wp-content/uploads/ok.png" width="20" height="20"></img></button>;
 };
